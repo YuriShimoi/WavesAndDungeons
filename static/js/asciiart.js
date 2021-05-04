5407bdf9b7b090539b01bed134c6f59d5d05f6dc
Yuri Shimoi <shimoi.yuri@gmail.com>
2021-05-04 10:06:17 -0300

    atualized lib, diagonalBlinkDiv working well

---
+++ static/js/asciiart.js
@@ -0,0 +1,22 @@
+// http://www.network-science.de/ascii/
+// roman font
+const CHARSET_TITLE_1 = "\
+oooooo   oooooo     oooo                                                                            .o8  \
+ `888.    `888.     .8'                                                                            ¬888  \
+  `888.   .8888.   .8'    .oooo.   oooo    ooo  .ooooo.   .oooo.o        .oooo.   ooo. .oo.    .oooo888  \
+   `888  .8'`888. .8'    `P  )88b   `88.  .8'  d88' `88b d88(  ¬8       `P  )88b  `888P¬Y88b  d88' `888  \
+    `888.8'  `888.8'      .oP¬888    `88..8'   888ooo888 `¬Y88b.         .oP¬888   888   888  888   888  \
+     `888'    `888'      d8(  888     `888'    888    .o o.  )88b       d8(  888   888   888  888   888  \
+      `8'      `8'       `Y888¬¬8o     `8'     `Y8bod8P' 8¬¬888P'       `Y888¬¬8o o888o o888o `Y8bod88P¬ \
+".replaceAll('¬','"');
+const CHARSET_TITLE_2 = "\
+         oooooooooo.                                                                                     \
+         `888'   `Y8b                                                                                    \
+          888      888 oooo  oooo  ooo. .oo.    .oooooooo  .ooooo.   .ooooo.  ooo. .oo.    .oooo.o       \
+          888      888 `888  `888  `888P¬Y88b  888' `88b  d88' `88b d88' `88b `888P¬Y88b  d88(  ¬8       \
+          888      888  888   888   888   888  888   888  888ooo888 888   888  888   888  `¬Y88b.        \
+          888     d88'  888   888   888   888  `88bod8P'  888    .o 888   888  888   888  o.  )88b       \
+         o888bood8P'    `V88V¬V8P' o888o o888o `8oooooo.  `Y8bod8P' `Y8bod8P' o888o o888o 8¬¬888P'       \
+                                               d¬     YD                                                 \
+                                               ¬Y88888P'                                                 \
+".replaceAll('¬','"');
--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,5 @@
-// http://www.network-science.de/ascii/
-// roman font
-const CHARSET_TITLE = "\
-oooooo   oooooo     oooo                                                                            .o8  \
- `888.    `888.     .8'                                                                            ¬888  \
-  `888.   .8888.   .8'    .oooo.   oooo    ooo  .ooooo.   .oooo.o        .oooo.   ooo. .oo.    .oooo888  \
-   `888  .8'`888. .8'    `P  )88b   `88.  .8'  d88' `88b d88(  ¬8       `P  )88b  `888P¬Y88b  d88' `888  \
-    `888.8'  `888.8'      .oP¬888    `88..8'   888ooo888 `¬Y88b.         .oP¬888   888   888  888   888  \
-     `888'    `888'      d8(  888     `888'    888    .o o.  )88b       d8(  888   888   888  888   888  \
-      `8'      `8'       `Y888¬¬8o     `8'     `Y8bod8P' 8¬¬888P'       `Y888¬¬8o o888o o888o `Y8bod88P¬ \
-                                                                                                         \
-         oooooooooo.                                                                                     \
-         `888'   `Y8b                                                                                    \
-          888      888 oooo  oooo  ooo. .oo.    .oooooooo  .ooooo.   .ooooo.  ooo. .oo.    .oooo.o       \
-          888      888 `888  `888  `888P¬Y88b  888' `88b  d88' `88b d88' `88b `888P¬Y88b  d88(  ¬8       \
-          888      888  888   888   888   888  888   888  888ooo888 888   888  888   888  `¬Y88b.        \
-          888     d88'  888   888   888   888  `88bod8P'  888    .o 888   888  888   888  o.  )88b       \
-         o888bood8P'    `V88V¬V8P' o888o o888o `8oooooo.  `Y8bod8P' `Y8bod8P' o888o o888o 8¬¬888P'       \
-                                               d¬     YD                                                 \
-                                               ¬Y88888P'\
-".replaceAll('¬','"');
+// VARIABLES
+
 
 
 // FUNCTIONS
@@ -26,33 +7,45 @@ function startGame(){
   // change base color to #4dd5ff
   setBaseColor("#4dd5ff");
 
-  newElement('div', {id:'test', type:'none', text:CHARSET_TITLE, width:105, height:17, x:'center', y:'center'}, 'main');
-  delElement('start_button');
+  newElement('div', {id:'title_case', type:'none', width:107, height:19, x:'center', y:'center'}, 'main', draw=false);
+  newElement('div', {id:'title_p1', type:'none', text:CHARSET_TITLE_1, width:105, height:7, x:'center'}, 'title_case', draw=false);
+  newElement('div', {id:'title_p2', type:'none', text:CHARSET_TITLE_2, width:105, height:9, x:'center', yalign:'bottom', color:'#2d85bd'}, 'title_case', draw=false);
+  delElement('start_button', draw=false);
+
+  diagonalBlinkDiv('title_p1', '~', 5, 2);
+  diagonalBlinkDiv('title_p2', '~', 5, 2);
 }
 
 function setBaseColor(color){
   document.getElementsByTagName("prompt")[0].setAttribute('style',`color:${color}`);
 }
 
-function stressTest(){
-  let counter  = 0;
-  aux = 0;
+function diagonalBlinkDiv(eid, char="*", delay=50, angle=2){
+  let el  = document.getElementById(eid);
+  let txt = el.getAttribute("text");
+  let wid = parseInt(el.getAttribute("width"));
+  let hei = parseInt(el.getAttribute("height"));
+
+  let step = 0;
   let interval = setInterval(() => {
-    let start = new Date();
-    if(counter++ == 100) {
+    if(step++ >= wid+(hei*angle)) {
       clearInterval(interval);
-      newElement('div', {x:'center',y:'center', width:6, height:1, text:`${(aux/100).toFixed(3)}s`}, 'main');
+      changeElement(eid, {text: txt});
       return;
     }
+
+    let btext = '';
+    for(let y=0; y<hei; y++){
+      let ypos = step < y*angle? wid * y: (step - y*angle) + (wid * y);
+      if(ypos >= wid*(y+1)) {
+        btext += txt.substring(wid*y, wid*(y+1));
+        continue;
+      }
+      btext += txt.substring(wid*y, ypos);
+      btext += txt[ypos] != " "? char: txt[ypos];
+      btext += ' '.repeat((wid*(y+1)) - (ypos+1));
+    }
     
-    let rx = Math.floor(Math.random() * 142);
-    let ry = Math.floor(Math.random() *  27);
-
-    let red = (Math.floor((Math.random() * 100 + 150) * Math.random())).toString(16);
-    let gre = (Math.floor((Math.random() * 100 + 150) * Math.random())).toString(16);
-    let blu = (Math.floor((Math.random() * 100 + 150) * Math.random())).toString(16);
-
-    newElement('div', {x:rx, y:ry, width:10, height:4, 'border-color':`#${red+gre+blu}`, color:`#${red+gre+blu}`, text:counter}, 'main');
-    aux += (new Date() - start)/1000;
-  }, 1);
+    changeElement(eid, {text: btext});
+  }, delay);
 }
