84372f380db261841bec40c95ea5939d9a856e40
Yuri Shimoi <shimoi.yuri@gmail.com>
2021-05-08 20:22:12 -0300

    lib atualization, added vertical separators

--- static/js/main.js
+++ static/js/main.js
@@ -4,23 +4,36 @@
 
 // FUNCTIONS
 function startGame(){
-  // change base color to #5de5ff
   setBaseColor("#ffffff");
 
+  delElement('start_button');
   newElement('div', {id:'title_case', type:'none', width:107, height:19, x:'center', y:'center'}, 'main', draw=false);
   newElement('div', {id:'title_p1',   type:'none', text:CHARSET_TITLE_1, width:105, height:7, x:'center', color:'#5de5ff'}, 'title_case', draw=false);
-  newElement('div', {id:'title_p2',   type:'none', text:CHARSET_TITLE_2, width:105, height:9, x:'center', yalign:'bottom', color:'#2d85bd'}, 'title_case', draw=false);
-  delElement('start_button', draw=false);
 
-  diagonalBlinkDiv('title_p1', '~', 5, 2);
-  diagonalBlinkDiv('title_p2', '~', 5, 2);
+  diagonalBlinkDiv('title_p1', '*', 5, 2, () => {
+    newElement('div', {id:'title_p2',   type:'none', text:CHARSET_TITLE_2, width:105, height:9, x:'center', yalign:'bottom', color:'#2d85bd'}, 'title_case', draw=false);
+    diagonalBlinkDiv('title_p2', '*', 5, 2, () => {
+      setTimeout(() => {
+        delElement('title_case', {hidden: 'true'});
+        showInterface();
+      }, 2000);
+    });
+  });
 }
 
+function showInterface(){
+  newElement('div', {id:'inventory', type:'double', height:29, width:30}, 'main');
+  newElement('div', {id:'map', type:'double', height:29, width:100, xalign:'right'}, 'main');
+  newElement('div', {id:'sep1', type:'none', clip:'true', width:7, height:31, x:32, y:0, text:VERTICAL_SEPARATOR2}, 'main')
+}
+
+
+// TOOLS
 function setBaseColor(color){
   document.getElementsByTagName("prompt")[0].setAttribute('style',`color:${color}`);
 }
 
-function diagonalBlinkDiv(eid, char="*", delay=50, angle=2){
+function diagonalBlinkDiv(eid, char="*", delay=50, angle=2, end_call=()=>{}){
   let el  = document.getElementById(eid);
   let txt = el.getAttribute("text");
   let wid = parseInt(el.getAttribute("width"));
@@ -31,6 +44,7 @@ function diagonalBlinkDiv(eid, char="*", delay=50, angle=2){
     if(step++ >= wid+(hei*angle)) {
       clearInterval(interval);
       changeElement(eid, {text: txt});
+      end_call();
       return;
     }
 
